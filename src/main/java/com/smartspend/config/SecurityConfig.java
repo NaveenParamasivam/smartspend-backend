45fe919743edaa0293d504bb49775687157b4de1
Naveen <naveenneevan73@gmail.com>
2026-04-08 15:42:55 +0530

    fixed access issue1

--- src/main/java/com/smartspend/config/SecurityConfig.java
+++ src/main/java/com/smartspend/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
-//                .requestMatchers("/ws/**").permitAll()
+                .requestMatchers("/ws/**").permitAll()
 //                            .requestMatchers("/api/auth/**").permitAll()
 //                    .requestMatchers("/auth/login", "/auth/register").permitAll()
 //                    .requestMatchers("/auth/me").authenticated()
